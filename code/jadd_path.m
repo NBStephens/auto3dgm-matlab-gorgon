4480cc64d6ebe4efe64b0fd60b2a95d115089fb9
Julie Winchester <julia.m.winchester@gmail.com>
2017-01-18 13:05:18 -0500

    New function: PCA of partial procrustes tangent projection coordinates

--- code/jadd_path.m
+++ code/jadd_path.m
@@ -24,6 +24,7 @@ email_notification = '';
 
 %%%%% experimental parameters
 align_to = 'auto';
+do_tangent_pca = 1;
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%% do not modify anything beyond this point
