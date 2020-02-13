85607c468c42e82a4e225d44a898800e0f3effe8
Nick Stephens <irreverentideas@gmail.com>
2020-02-13 08:59:10 -0500

    Update so scale, rotation matrix, global matrix, and center are all writen out for the indivdual files.

--- code/write_obj_aligned_shapes.m
+++ code/write_obj_aligned_shapes.m
@@ -19,6 +19,9 @@ function write_obj_aligned_shapes(ds, ga)
 % Email: jmw110@duke.edu
 % Website: http://www.apotropa.com
 % January 5, 2017
+% Modified by Nicholas Stephens, Dr. rer. nat.
+% Email: nbs49@psu.edu
+% February, 13, 2020
 
 center = @(X) X-repmat(mean(X,2),1,size(X,2));
 scale  = @(X) norm(center(X),'fro') ;
@@ -26,18 +29,28 @@ scale  = @(X) norm(center(X),'fro') ;
 centeredV = cellfun(@(X) center(X.origV), ds.shape, 'UniformOutput', false);
 vertexArray = cellfun(@(V,R) R * (V/scale(V)), centeredV, ga.R, 'UniformOutput', false);
 
+
 if ds.refAlign == 1
 	coordR = coord_axis_rotation(vertexArray);
-	vertexArray = cellfun(@(X) coordR * X, vertexArray, 'UniformOutput', false); 
+	vertexArray = cellfun(@(X) coordR * X, vertexArray, 'UniformOutput', false);
+    global_matrix = array2table(coordR, ...
+        'VariableNames', {'x', 'y', 'z'});
+    writetable(global_matrix, [ds.msc.mesh_aligned_dir, 'global_matrix.txt']);
+    
 end
 
 for ii = 1:ds.n
     cen_rot = array2table(ds.shape{ii}.center, ...
         'VariableNames', {'Center'});
     writetable(cen_rot, [ds.msc.mesh_aligned_dir ds.names{ii}, '_center.csv']);
+    scale = array2table(ds.shape{ii}.scale, ...
+        'VariableNames', {'Scale'});
+    writetable(scale, [ds.msc.mesh_aligned_dir ds.names{ii}, '_scale.csv']);write
     rot_mat = array2table(ga.R{ii}, ...
         'VariableNames', {'x', 'y', 'z'});
     writetable(rot_mat, [ds.msc.mesh_aligned_dir ds.names{ii}, '_rotation_matrix.txt']);
+        
+
     
 end
     
